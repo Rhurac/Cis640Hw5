86ec40e6378c9e24ab62784b2ed31df2db492fc5
Zachary <zcleary1@ksu.edu>
2017-04-14 00:22:31 -0500

    All initial tests passed

--- impl.py
+++ impl.py
@@ -1,14 +1,25 @@
 class Queue(object):
-
     def __init__(self):   # default no-arg constructor
+        self.q = []
         pass
 
-    def enqueue(self, v):  # adds v to the back of the queue; no return value
+    def enqueue(self, v): # adds v to the back of the queue; no return value
+        if v is None:
+            raise ValueError
+        self.q.append(v)
+        print(self.q)
         pass
 
-    def dequeue(self):    # removes and returns the element at the front of the queue
+    def dequeue(self): # removes and returns the element at the front of the queue
+        if (len(self.q) > 0):
+            element = self.q.pop()
+            self.q.reverse()
+            self.q.reverse()
+            return element
+        else:
+            return None
         pass
 
     def len(self):        # returns the number of elements in the queue
-        # assert isinstance(ret_val, int)
+        return len(self.q)
         pass
--- test_impl.py
+++ test_impl.py
@@ -4,47 +4,127 @@ import hypothesis.strategies as st
 from hypothesis import given
 
 
+# noinspection PyBroadException
 class TestQueue:
     """ Tests for the properties of a Queue """
 
     # A strategy which generates only valid inputs that can be queued
-    strategy = st.lists(elements=(st.floats(allow_nan=False, allow_infinity=False) | st.text() | st.integers() | st.booleans()))
+    strat_valid_input = st.lists(elements=(st.floats(allow_nan=False, allow_infinity=False) | st.text() | st.integers() | st.booleans()))
 
-    @given(strategy)
-    def test_popped_item_was_first_item_pushed(self, x):
+    @given(strat_valid_input)
+    def test_popped_item_was_first_item_pushed(self, lst):
         """ If len() > 0 and x = pop(), then x was the first item pushed and remove x from Queue """
-        pass
+        try:
+            q = Queue()
+            for x in lst:
+                q.enqueue(x)
+            while q.len() > 1:
+                q.dequeue()
+            if len(lst) > 0:
+                assert q.dequeue() is lst[0]
+        except:
+            assert False
 
-    def test_empty_queue_is_length_zero(self):
-        """ If nothing has been placed on the queue, then len() = 0 """
-        pass
+    @given(strat_valid_input)
+    def test_empty_queue_is_length_zero(self, lst):
+        """ If nothing has been placed on the queue, then length is zero """
+        try:
+            q = Queue()
+            for x in lst:
+                q.enqueue(x)
+            while q.len() > 0:
+                q.dequeue()
+            assert q.len() == 0
+        except:
+            assert False
 
-    def test_pop_decrements_count(self):
+    @given(strat_valid_input)
+    def test_pop_decrements_count(self, lst):
         """ Calls to a successful pop should decrement the length by one """
-        pass
+        try:
+            q = Queue()
+            for x in lst:
+                q.enqueue(x)
+            for i in range(1, len(lst)):
+                q.dequeue()
+                assert q.len() == len(lst) - i
+        except:
+            assert False
 
-    def test_push_increments_count(self):
+    @given(strat_valid_input)
+    def test_push_increments_count(self, lst):
         """ Calls to successful push should increment the length by one """
         pass
 
-    def test_pop_empty_queue_returns_none(self):
+    @given(strat_valid_input)
+    def test_pop_empty_queue_returns_none(self, lst):
         """ Popping an empty queue should return None """
-        pass
+        try:
+            q = Queue()
+            for x in lst:
+                q.enqueue(x)
+            while q.len() > 0:
+                q.dequeue()
+            assert q.dequeue() is None
+        except:
+            assert False
 
     def test_push_none_raises_exception(self):
         """ Pushing value None raises a ValueError exception """
-        pass
+        try:
+            q = Queue()
+            q.enqueue(None)
+            assert False
+        except ValueError:
+            assert True
+        except:
+            assert False
 
-    def test_popping_empty_queue_does_not_change_length(self):
+    @given(strat_valid_input)
+    def test_popping_empty_queue_does_not_change_length(self, lst):
         """ Popping and empty queue does not change the length """
-        pass
+        try:
+            q = Queue()
+            cnt = len(lst)
+            for x in lst:
+                q.enqueue(x)
+            while q.len() > 0:
+                q.dequeue()
+            assert q.len() == 0
+            q.dequeue()
+            assert q.len() == 0
+        except:
+            assert False
 
-    def test_popping_nonempty_queue_does_not_return_none(self):
+    @given(strat_valid_input)
+    def test_popping_nonempty_queue_does_not_return_none(self, lst):
         """ Popping a queue with length greater than zero returns an object """
-        pass
+        try:
+            q = Queue()
+            for x in lst:
+                q.enqueue(x)
+            while q.len() > 0:
+                assert q.dequeue() is not None
+        except:
+            assert False
 
-    def test_length_of_queue(self):
+    @given(st.lists(min_size=1, elements=(st.floats(allow_nan=False, allow_infinity=False) | st.text() | st.integers() | st.booleans())))
+    def test_length_of_queue(self, lst):
         """ The length of the queue should be equal to number of pushes minus pops """
-        pass
+        try:
+            q = Queue()
+            pushes = 0
+            pops = 0
+            for x in lst:
+                q.enqueue(x)
+                pushes = pushes + 1
+                assert q.len() == pushes - pops
+            while q.len() > 0:
+                q.dequeue()
+                pops = pops + 1
+                assert q.len() == pushes - pops
+        except:
+            assert False
+
 
 
